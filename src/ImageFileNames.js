37dbfec5c98de1ab7a71a32c6298528143dece30
bemmons1993 <61093072+bemmons1993@users.noreply.github.com>
2020-08-03 21:21:28 -0500

    had to change repo due to deployment errors

---
+++ src/ImageFileNames.js
@@ -0,0 +1,3 @@
+let ImageFileNames = ["billy.png", "brule.png", "cartman.png", "casey.png", "chris.png", "claire.png", "dwight.png", "jenna.png", "jim.png", "kenny.png", "kyle.png", "paul.png", "spaget.png", "steve.png", "timanderic.png", "vaughn.png"];
+
+export default ImageFileNames;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer text-center">
+    <span>Clicky Game © 2018 Copyright:&nbsp;<a href="https://github.com/bemmons1993" target="_blank" rel="noopener noreferrer">Brian Emmons</a></span>
+  </footer>
+);
+
+export default Footer;
